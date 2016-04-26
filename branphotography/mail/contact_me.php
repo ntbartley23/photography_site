fadd60105a70234d3544cf3f021179088700705e
Najee <najee@Najees-MBP.fios-router.home>
2016-04-25 22:26:56 -0400

    added email

--- branphotography/mail/contact_me.php
+++ branphotography/mail/contact_me.php
@@ -14,9 +14,9 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'me@myprogrammingblog.com'; // put your email
+$to = 'bran.bartley@gmail.com'; // put your email
 $email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. \n\n".
+$email_body = "You have received a new website message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message \n $message";
 $headers = "From: contacts@myprogrammingblog.com\n";
